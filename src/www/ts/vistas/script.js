e39a95bcb3fc15028536dc84ae69d92b3098a2f5
lauramerinoortiz <lauramerinoortiz.guadalupe@alumnado.fundacionloyola.net>
2023-02-14 09:13:25 +0100

    Vista ok

--- src/www/ts/vistas/script.js
+++ src/www/ts/vistas/script.js
@@ -1,3 +1,35 @@
+define("vista", ["require", "exports"], function (require, exports) {
+    "use strict"; //activo modo estricto
+    exports.__esModule = true;
+    exports.Vista = void 0;
+    /**
+     * Clase Vista con métodos y atributos general que tendrán todas las Vistas
+    */
+    var Vista = /** @class */ (function () {
+        /**
+         * Contructor de la clase Vista
+         * @param {Objeto} divinicio div de la vista
+         */
+        function Vista(divinicio) {
+            this.div = divinicio;
+        }
+        /**
+         * Método mostrar que pone el div de la Vista visualizándose u ocultándolo
+         * @param {boolean} ver recibe un true o un false
+         */
+        Vista.prototype.mostrar = function (ver) {
+            if (ver) {
+                this.div.style.display = 'block';
+                console.log('mostrando ', this.div);
+            }
+            else {
+                this.div.style.display = 'none';
+            }
+        };
+        return Vista;
+    }());
+    exports.Vista = Vista;
+});
 define("pelicula", ["require", "exports"], function (require, exports) {
     "use strict"; //activo modo estricto
     exports.__esModule = true;
--- src/www/ts/vistas/vista.ts
+++ src/www/ts/vistas/vista.ts
@@ -3,11 +3,12 @@
  * Clase Vista con métodos y atributos general que tendrán todas las Vistas 
 */
 export class Vista{
+	public div:HTMLDivElement
 	/**
 	 * Contructor de la clase Vista
 	 * @param {Objeto} divinicio div de la vista
 	 */
-	constructor(divinicio){
+	constructor(divinicio:HTMLDivElement){
 		this.div=divinicio
 	}
 	
@@ -15,7 +16,7 @@ export class Vista{
 	 * Método mostrar que pone el div de la Vista visualizándose u ocultándolo
 	 * @param {boolean} ver recibe un true o un false
 	 */
-	mostrar(ver){
+	mostrar(ver:boolean){
 		if(ver){
 			this.div.style.display='block'
 			console.log('mostrando ', this.div)
